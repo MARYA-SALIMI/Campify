75652d072df80e0064f26d4cdbda138f7d1ff209
Sinem Havan <github-hesabini-actigin-havansinem@gmail.com>
2026-03-31 20:48:06 +0300

    feat: ana sayfa tasarımı, dark mode ve yeni gönderi modalı eklendi

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-/*import Home from "../pages/Home";
-import ProfilePage from "../pages/ProfilePage";
+import Home from "../pages/Home";
+/*import ProfilePage from "../pages/ProfilePage";
 import PostPage from "../pages/PostPage";
 import MessagePage from "../pages/MessagePage";*/
 import TeamPage from "../pages/TeamPage";
@@ -8,13 +8,13 @@ import TeamPage from "../pages/TeamPage";
 function AppRoutes() {
   return (
     <Routes>
-      {
-      /*<Route path="/" element={<Home />} />
-      <Route path="/post" element={<PostPage />} />
+     
+      <Route path="/" element={<Home />} />
+      {/*<Route path="/post" element={<PostPage />} />
       <Route path="/messages" element={<MessagePage />} />
-      <Route path="/profile" element={<ProfilePage />} />*/
+      <Route path="/profile" element={<ProfilePage />} />*/}
       <Route path="/team" element={<TeamPage />} />
-      }
+      
     </Routes>
   );
 }
