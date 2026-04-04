13ff057bf2b6f08876a8964f14e4d2dc83029794
Sinem Havan <github-hesabini-actigin-havansinem@gmail.com>
2026-04-04 17:21:42 +0300

    rota hatasi duzeltildi

--- server/src/services/chatService.js
+++ server/src/services/chatService.js
@@ -2,29 +2,24 @@ const mongoose = require('mongoose');
 const Chat = require('../models/Chat');
 const Message = require('../models/Message');
 
+// 1. Yeni sohbet oluşturma
+exports.createChat = async (participants) => {
+  const newChat = new Chat({ participants });
+  return await newChat.save();
+};
+
+// 2. Kullanıcının sohbetlerini listeleme
 exports.getUserChats = async (userId) => {
   return await Chat.find({
     participants: new mongoose.Types.ObjectId(userId)
   }).sort({ updatedAt: -1 });
 };
 
-const Chat = require('../models/Chat');
-const Message = require('../models/Message');
-
-// 1. Yeni sohbet oluşturma (Mesaj atabilmemiz için bir odaya ihtiyacımız var)
-exports.createChat = async (participants) => {
-  const newChat = new Chat({ participants });
-  return await newChat.save();
-};
-
-
-// 3. Belirli bir sohbete mesaj gönderme (POST /chats/:chatId/messages)
+// 3. Mesaj gönderme
 exports.sendMessage = async (chatId, senderId, content) => {
-  // A. Önce mesajı oluştur ve kaydet
   const newMessage = new Message({ chatId, senderId, content });
   const savedMessage = await newMessage.save();
 
-  // B. Mesaj başarıyla kaydedilirse, ilgili sohbetin 'lastMessage' bilgisini güncelle
   await Chat.findByIdAndUpdate(chatId, { lastMessage: savedMessage._id });
 
   return savedMessage;
